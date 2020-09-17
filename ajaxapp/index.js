8a0dd45c6c7a5b57435deec06c586246da66626d
mizobuchiy <mizobuchi2789@gmail.com>
2020-09-18 00:51:30 +0900

    ajaxapp

---
+++ ajaxapp/index.js
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+async function main() {
+  try {
+    const userId = getUserID();
+    const userInfo = await fetchUserInfo(userId);
+    const view = createView(userInfo);
+    displayView(view);
+  } catch (error) {
+    console.error(`エラーが発生しました (${error})`);
+  }
+}
+
+/**
+ * 
+ * @param {*} userId 
+ */
+function fetchUserInfo(userId) {
+  return fetch(
+    `https://api.github.com/users/${encodeURIComponent(userId)}`
+  ).then((response) => {
+    if (!response.ok) {
+      return Promise.reject(
+        new Error(`${response.status}: ${response.statusText}`)
+      );
+    } else {
+      return response.json();
+    }
+  });
+}
+/**
+ * 
+ */
+function getUserID() {
+  return document.getElementById("userId").value;
+}
+
+/**
+ * 
+ * @param {*} userInfo 
+ */
+function createView(userInfo) {
+  return escapeHTML`
+                    <h4>${userInfo.name} (@${userInfo.login})</h4>
+                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+                    <dl>
+                        <dt>Location</dt>
+                        <dd>${userInfo.location}</dd>
+                        <dt>Repositories</dt>
+                        <dd>${userInfo.public_repos}</dd>
+                    </dl>
+                    `;
+}
+
+function displayView(view) {
+  const result = document.getElementById("result");
+  result.innerHTML = view;
+}
+
+/**
+ * 
+ * @param {*} str 
+ */
+function escapeSpecialChars(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+/**
+ * 
+ * @param {*} strings 
+ * @param  {...any} values 
+ */
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+}
